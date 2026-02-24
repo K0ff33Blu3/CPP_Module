5e8928a510702c2546fe562a2c3d911872d1db35
miricci42 <miricci@student.42firenze.it>
2026-02-24 17:20:16 +0100

    aggiunti file ex02

--- module_02/ex02/Fixed.hpp
+++ module_02/ex02/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: miricci <miricci@student.42firenze.it>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/19 17:01:07 by miricci           #+#    #+#             */
-/*   Updated: 2026/02/24 11:47:33 by miricci          ###   ########.fr       */
+/*   Updated: 2026/02/24 14:14:52 by miricci          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,28 @@
 # define FIXED_HPP
 
 # include <iostream>
+# include <cmath>
 
 class Fixed
 {
 	private:
-		int					raw_value;
+		int	raw_value;
 		static const int	bp = 8;
 	public:
 		Fixed( void );
+		Fixed( const int nbr );
+		Fixed( const float nbr );
 		Fixed( const Fixed& other );
 		Fixed& operator=( const Fixed& other );
 		~Fixed( void );
-
-		int		getRawBits( void ) const;
+		
+		int	getRawBits( void ) const;
 		void	setRawBits( int const raw );
+		
+		float	toFloat( void ) const;
+		int		toInt( void ) const;
 };
+
+std::ostream& operator<<( std::ostream& o, const Fixed& f );
 
 #endif
--- module_02/ex02/main.cpp
+++ module_02/ex02/main.cpp
@@ -1,33 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: miricci <miricci@student.42firenze.it>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/02/19 17:01:07 by miricci           #+#    #+#             */
-/*   Updated: 2026/02/24 11:47:33 by miricci          ###   ########.fr       */
+/*   Created: 2026/02/24 14:40:16 by miricci           #+#    #+#             */
+/*   Updated: 2026/02/24 14:40:24 by miricci          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FIXED_HPP
-# define FIXED_HPP
+#include "Fixed.hpp"
 
-# include <iostream>
+int main( void ) {
 
-class Fixed
-{
-	private:
-		int					raw_value;
-		static const int	bp = 8;
-	public:
-		Fixed( void );
-		Fixed( const Fixed& other );
-		Fixed& operator=( const Fixed& other );
-		~Fixed( void );
-
-		int		getRawBits( void ) const;
-		void	setRawBits( int const raw );
-};
-
-#endif
+	Fixed a;
+	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << Fixed::max( a, b ) << std::endl;
+	return (0);
+}
